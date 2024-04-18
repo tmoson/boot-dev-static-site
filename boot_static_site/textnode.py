f4ffd3037d741a5a52d7b6b58b15ce8667c4733e
Tyler Moson <tyler.p.moson@gmail.com>
2024-04-17 22:58:06 -0400

    fixing some missed requirements

--- boot_static_site/textnode.py
+++ boot_static_site/textnode.py
@@ -1,7 +1,7 @@
 
 class TextNode:
 
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
--- tests/test_textnode.py
+++ tests/test_textnode.py
@@ -4,6 +4,10 @@ from boot_static_site.textnode import TextNode
 
 class TestTextNode():
     def test_eq(self):
-        node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-        node2 = TextNode("This is a text node", "bold", "https://www.boot.dev")
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
         assert node.eq(node2)
+
+    def test_creation(self):
+        node = TextNode("This is a text node", "bold")
+        assert node.repr() == "TextNode(This is a text node, bold, None)"
