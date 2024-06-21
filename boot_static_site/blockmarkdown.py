41bab0c21c02e313c90d38ce1c25a1b0bb46aa2a
Tyler Moson <tyler.p.moson@gmail.com>
2024-06-20 22:58:35 -0400

    created block_to_block_type function, working on tests

--- boot_static_site/blockmarkdown.py
+++ boot_static_site/blockmarkdown.py
@@ -1,18 +1,45 @@
 import re
 from .textnode import TextNode, text_to_textnodes
 
-type_header = "header"
-type_list = "list"
+type_heading = "heading"
+type_ordered_list = "ordered_list"
+type_unordered_list = "unordered_list"
 type_paragraph = "paragraph"
-        
-def get_block_type(text: str) -> str:
-    match text[0]:
-        case "#":
-            return type_header + str(len(re.match(r"^(#+)", text).group(0)))
-        case "*":
-            return type_list
-        case _:
-            return type_paragraph
+type_code = "code"
+type_quote = "quote"
+
+
+def block_to_block_type(block):
+    if re.match(r'^#{1,6} ([A-z]|[1-9])+', block) is not None:
+        return type_heading
+    elif block.startswith("```\n") and block.endswith("\n```"):
+        return type_code
+    elif block.startswith("1."):
+        num = 0
+        for line in block.splitlines():
+            num += 1
+            if re.match(r'^(\d+\.)', line) is None:
+                return type_paragraph
+            elif int(re.match(r'^(\d+)', line).group(0)) != num:
+                return type_paragraph
+        return type_ordered_list
+    elif block.startswith("* "):
+        for line in block.splitlines():
+            if not line.startswith("* "):
+                return type_paragraph
+        return type_unordered_list
+    elif block.startswith("- "):
+        for line in block.splitlines():
+            if not line.startswith("- "):
+                return type_paragraph
+        return type_unordered_list
+    elif block.startswith(">"):
+        for line in block.splitlines():
+            if not line.startswith(">"):
+                return type_paragraph
+        return type_quote
+    else:
+        return type_paragraph
         
 def markdown_to_blocks(markdown):
     if markdown is None or markdown == "":
--- tests/test_blockmarkdown.py
+++ tests/test_blockmarkdown.py
@@ -1,6 +1,15 @@
 import pytest
 
-from boot_static_site.blockmarkdown import markdown_to_blocks
+from boot_static_site.blockmarkdown import (
+    type_heading,
+    type_code,
+    type_ordered_list,
+    type_unordered_list,
+    type_quote,
+    type_paragraph,
+    block_to_block_type,
+    markdown_to_blocks
+)
 
 class TestBlockMarkdown:
     def test_markdown_to_blocks(self):
@@ -36,3 +45,100 @@ class TestBlockMarkdown:
         assert len(actual) == len(expected)
         for i in range(0, len(actual)):
             assert actual[i] == expected[i]
+
+    def test_block_to_block_type(self):
+        text = ("# This is a heading   "
+                "\n"
+                "\n"
+                "  This is a paragraph of text. It has some **bold** and *italic* wordsd inside of it.\n"
+                "\n"
+                "* This is a list item  \n"
+                "* This is another list item")
+        blocks = markdown_to_blocks(text)
+        expected = [ type_heading, type_paragraph, type_unordered_list ]
+        for i in range(0, len(blocks)):
+            assert block_to_block_type(blocks[i]) == expected[i]
+        text = ("This is a **bolded** paragraph\n"
+                "\n"
+                "This is another paragraph with *italic* text and `code` here\n"
+                "This is the same paragraph on a new line\n"
+                "\n"
+                "* This is a list\n"
+                "* with items")
+        blocks = markdown_to_blocks(text)
+        expected = [ type_paragraph, type_paragraph, type_unordered_list ]
+        for i in range(0, len(blocks)):
+            assert block_to_block_type(blocks[i]) == expected[i]
+
+    def test_block_to_block_type_quote(self):
+        text = ("This is a paragraph\n"
+                "\n"
+                "This is another paragraph\n"
+                "\n"
+                "> This is the beginning of a blockquote\n"
+                "> This is the second line of said quote\n"
+                "> Here is the final line\n"
+                "\n"
+                "# We're adding a header here now")
+        blocks = markdown_to_blocks(text)
+        expected = [ type_paragraph, type_paragraph, type_quote, type_heading ]
+        for i in range(0, len(blocks)):
+            assert block_to_block_type(blocks[i]) == expected[i]
+        text = ("This is a paragraph\n"
+                "\n"
+                "This is another paragraph\n"
+                "\n"
+                "> This is the beginning of a blockquote\n"
+                "oops, I forgot a '>' at the start of this line\n"
+                "> Here is the final line\n"
+                "\n"
+                "# We're adding a header here now")
+        blocks = markdown_to_blocks(text)
+        expected = [ type_paragraph, type_paragraph, type_paragraph, type_heading ]
+        for i in range(0, len(blocks)):
+            assert block_to_block_type(blocks[i]) == expected[i]
+        
+
+    def test_block_to_block_type_unordered_list(self):
+        text = ("some text to start\n"
+                "\n"
+                "* a reasonable list item\n"
+                "* see that I'm being consistent with my bullets\n"
+                "\n"
+                "# That mean's I'll have a nice unordered list")
+        blocks = markdown_to_blocks(text)
+        expected = [ type_paragraph, type_unordered_list, type_heading ]
+        for i in range(0, len(blocks)):
+            assert block_to_block_type(blocks[i]) == expected[i]
+        text = ("some text to start\n"
+                "\n"
+                "- a reasonable list item\n"
+                "- see that I'm being consistent with my bullets\n"
+                "- Testing with '-' now to be sure both bullets work\n"
+                "\n"
+                "# That mean's I'll have a nice unordered list")
+        blocks = markdown_to_blocks(text)
+        expected = [ type_paragraph, type_unordered_list, type_heading ]
+        for i in range(0, len(blocks)):
+            assert block_to_block_type(blocks[i]) == expected[i]
+        text = ("some text to start\n"
+                "\n"
+                "- a reasonable list item\n"
+                "* I'm being inconsistent now. this isn't the way to make a list\n"
+                "- This is going to just be a paragraph now\n"
+                "\n"
+                "# But this will still be a heading")
+        blocks = markdown_to_blocks(text)
+        expected = [ type_paragraph, type_paragraph, type_heading ]
+        for i in range(0, len(blocks)):
+            assert block_to_block_type(blocks[i]) == expected[i]
+        text = ("some text to start\n"
+                "\n"
+                "- a reasonable list item\n"
+                "-This is going to just be a paragraph now\n"
+                "\n"
+                "# But this will still be a heading")
+        blocks = markdown_to_blocks(text)
+        expected = [ type_paragraph, type_paragraph, type_heading ]
+        for i in range(0, len(blocks)):
+            assert block_to_block_type(blocks[i]) == expected[i]
