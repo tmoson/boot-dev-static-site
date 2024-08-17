ff13197d74c3babb52a4b5666e62ab543ee47888
Tyler Moson <tyler.p.moson@gmail.com>
2024-08-16 21:46:37 -0400

    revert to default from prev step solution for cli compatibility

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+
+def main():
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
+
+
+main()
---
+++ test.sh
@@ -0,0 +1,1 @@
+python3 -m unitttest discover -s src/
