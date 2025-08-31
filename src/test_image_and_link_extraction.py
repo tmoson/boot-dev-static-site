922f63f919447494b62247d899d716f5a661aaca
tmoson <tyler.p.moson@gmail.com>
2025-08-30 23:01:33 -0400

    add some more tests

--- src/test_image_and_link_extraction.py
+++ src/test_image_and_link_extraction.py
@@ -14,6 +14,24 @@ class TestImageAndLinkExtraction(unittest.TestCase):
         )
         self.assertListEqual([('to boot dev', 'https://www.boot.dev'), ('to youtube', 'https://www.youtube.com/@bootdotdev')], matches)
 
+    def test_bad_extractions(self):
+        bad_images = [
+            "This is text with just a [link](https://www.boot.dev), but not an image link",
+            "This is text with a bad ![image(https://i.imgur.com/zjjcJKZ.png)",
+            "This is another bad !image(/path/to/png) reference.",
+            "This has one last missing ![element](https://i.imgur.com/zjjcJKZ.png"
+        ]
+        for image_text in bad_images:
+            self.assertListEqual([], extract_markdown_images(image_text))
+        bad_links = [
+            "This is a bad [link(https://www.boot.dev)",
+            "I just want to link to [(https://google.com)]",
+            "linking is broader than images like this(https://youtube.com)"
+        ]
+        for link_text in bad_links:
+            self.assertEqual([], extract_markdown_links(link_text))
+        self.assertListEqual([('image', 'https://i.imgur.com/zjjcJKZ.png')], extract_markdown_links("This is an ![image](https://i.imgur.com/zjjcJKZ.png)"))
+        
     def test_image_and_link(self):
         test_string = "This is text with a link to [to boot dev](https://www.boot.dev), an ![image](https://i.imgur.com/zjjcJKZ.png), and another link [to youtube](https://www.youtube.com/@bootdotdev). And one last ![image link](https://i.imgur.com/Pstbciu.jpeg)"
         images = extract_markdown_images(test_string)
